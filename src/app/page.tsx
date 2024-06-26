d2cfce32fe46cdd9f56ca244d4eb6d6b4ff68650
jhlee <jhlee@spicusinc.com>
2024-06-26 13:57:11 +0900

    edit service

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import speech_to_text_rest from "@/services/rest/speechtotext";
 import { Viseme } from "@/types/Viseme";
 import Script from "next/script";
 import { useRef } from "react";
 
 const Home = () => {
   const refVoice = useRef<HTMLSelectElement>(null);
-  const refPhrase = useRef<HTMLInputElement>(null);
+  const refExpressStyle = useRef<HTMLSelectElement>(null);
+  const refPhrase = useRef<HTMLTextAreaElement>(null);
   const refStartSpeakTextAsyncButton = useRef<HTMLButtonElement>(null);
   const refAudioTag = useRef<HTMLAudioElement>(null);
   const refAudioFile = useRef<HTMLAnchorElement>(null);
@@ -35,12 +35,16 @@ const Home = () => {
 
     // SSML 마크업을 음원으로 변환
     const ssml = `
-      <speak version='1.0' xml:lang='en-US'>
-        <voice xml:lang='en-US' name='${refVoice.current!.value}'>
-          ${refPhrase.current!.value}
+      <speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="https://www.w3.org/2001/mstts" xml:lang="en-US">
+        <voice name="${refVoice.current!.value}">
+          <mstts:express-as style="${refExpressStyle.current!.value}" styledegree="2">
+            ${refPhrase.current!.value}
+          </mstts:express-as>
         </voice>
       </speak>
+      
     `;
+    window.console.log(ssml);
     synthesizer.speakSsmlAsync(
       ssml,
       (result: any) => {
@@ -66,7 +70,7 @@ const Home = () => {
     
     // Viseme 수신 이벤트 함수   
     synthesizer.visemeReceived = (s: any, e: any) => {
-      window.console.log("(Viseme), Audio offset: " + e.audioOffset / 10000 + "ms. Viseme ID: " + e.visemeId);
+      //window.console.log("(Viseme), Audio offset: " + e.audioOffset / 10000 + "ms. Viseme ID: " + e.visemeId);
 
       // `Animation` is an xml string for SVG or a json string for blend shapes
       const animation = e.Animation;
@@ -94,20 +98,7 @@ const Home = () => {
     // Subscription Key 필드 Disabled 종료
     refStartSpeakTextAsyncButton.current!.disabled = false;
     
-    window.console.log(error);
-  };
-
-  const tts_rest_deprecated = async () => {
-    const ssml = `
-      <speak version='1.0' xml:lang='en-US'>
-        <voice xml:lang='en-US' xml:gender='Female' name='en-US-JennyNeural'>
-          ${refPhrase.current!.value}
-        </voice>
-      </speak>
-    `
-
-    const data = await speech_to_text_rest(SPEECH_KEY, SPEECH_REGION, ssml, "test.mp3");
-    download(data, refAudioFile, "audio/mp3", "audio.mp3");
+    //window.console.log(error);
   };
 
   const update_audio_src = (data: any) => {
@@ -123,21 +114,36 @@ const Home = () => {
   
   return (
     <main className="flex flex-col justify-center items-center min-h-screen">
-      <div className="flex flex-col justify-center items-center gap-4">
+      <div className="flex flex-col justify-center items-center gap-4 w-full max-w-xl h-full">
         <select
           ref={refVoice}
           className="w-full px-1 py-2 bg-transparent border-2 border-black dark:border-white"
         >
           <option className="p-2 bg-white dark:bg-black" value="en-US-JasonNeural">US: Jason</option>
           <option className="p-2 bg-white dark:bg-black" value="en-US-CoraNeural">US: Cora</option>
+          <option className="p-2 bg-white dark:bg-black" value="en-US-AndrewNeural">US: Andrew</option>
+          <option className="p-2 bg-white dark:bg-black" value="en-US-EmmaNeural">US: Emma</option>
+          <option className="p-2 bg-white dark:bg-black" value="en-US-GuyNeural">US: Guy</option>
+          <option className="p-2 bg-white dark:bg-black" value="en-US-SaraNeural">US: Sara</option>
           <option className="p-2 bg-white dark:bg-black" value="en-GB-RyanNeural">UK: Ryan</option>
           <option className="p-2 bg-white dark:bg-black" value="en-GB-AbbiNeural">UK: Abbi</option>
         </select>
-        <input
+        <select
+          ref={refExpressStyle}
+          className="w-full px-1 py-2 bg-transparent border-2 border-black dark:border-white"
+        >
+          <option className="p-2 bg-white dark:bg-black" value="cheerful">cheerful</option>
+          <option className="p-2 bg-white dark:bg-black" value="excited">excited</option>
+          <option className="p-2 bg-white dark:bg-black" value="hopeful">hopeful</option>
+          <option className="p-2 bg-white dark:bg-black" value="angry">angry</option>
+          <option className="p-2 bg-white dark:bg-black" value="depressed">depressed</option>
+        </select>
+        <textarea
           ref={refPhrase}
           placeholder="phrase"
           className="w-full p-2 bg-transparent border-2 border-black dark:border-white"
-        />
+          rows={8}
+        ></textarea>
         <button
           ref={refStartSpeakTextAsyncButton}
           className="w-full p-2 bg-transparent border-2 border-black dark:border-white"
